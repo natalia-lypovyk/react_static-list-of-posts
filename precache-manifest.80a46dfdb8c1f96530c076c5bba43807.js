b6b857d8c70df3b4aee237be9b1244ffe0301009
Natalia Lypovyk <cardinnata@gmail.com>
2020-02-14 01:35:03 +0200

    Updates

---
+++ precache-manifest.80a46dfdb8c1f96530c076c5bba43807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1788881ec0ad743555c49dd716bfb5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a3d77abc29096f0ea61",
+    "url": "./static/css/main.5b211374.chunk.css"
+  },
+  {
+    "revision": "ebf091a2f49af72b4477",
+    "url": "./static/js/2.c7ea46c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.c7ea46c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a3d77abc29096f0ea61",
+    "url": "./static/js/main.4622a83c.chunk.js"
+  },
+  {
+    "revision": "0005f2a639f1868a7e30",
+    "url": "./static/js/runtime-main.ef9b1eb6.js"
+  }
+]);
